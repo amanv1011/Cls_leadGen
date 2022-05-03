2196de19531248bc2dd754ff9e16b7512ad6ba39
Sahil <sahil.sondawale@classicinformatics.com>
2022-05-04 03:47:55 +0530

    code_destructure

--- src/components/commonComponents/lead/Approve.jsx
+++ src/components/commonComponents/lead/Approve.jsx
@@ -1,8 +1,8 @@
-import moment from "moment";
+
 import React from "react";
 
 import Cards from "./Cards";
-import Fuse from "fuse.js";
+
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getRejectCount } from "../../../redux/actions/approveRejectcount";
@@ -10,6 +10,8 @@ import { getUnderreviewCount } from "../../../redux/actions/approveRejectcount";
 import { getApproveCount } from "../../../redux/actions/approveRejectcount";
 import { getArchieveCount } from "../../../redux/actions/approveRejectcount";
 import { getAllCount } from "../../../redux/actions/approveRejectcount";
+import {filterLeads} from "../lead/filterLeads"
+import {filterCount} from "../lead/filterCount"
 import PopupBox from "./PopupBox";
 import "./lead.scss";
 const Approve = () => {
@@ -23,291 +25,48 @@ const Approve = () => {
   );
   const ownerNameFilter = useSelector((state) => state.leadsFilter.ownerName);
   const approveList = genratedLeadData.filter((ele) => ele.status === 1);
-
-  var filterApprovResults;
+  
+  
   var filterApprov;
-  var campaignID;
   var leadListForCount;
-  var campaignIdCount
-   //For Counting Leads
 
-   if (
-    campaignNameFilter === "All Campaigns" &&
-    ownerNameFilter === "All Owners"
-  ) {
-    leadListForCount = [];
-     leadListForCount = genratedLeadData;
-  }
-  if (
-    campaignNameFilter !== "All Campaigns" ||
-    ownerNameFilter !== "All Owners"
-  ) {
-     
-    if (
-      campaignNameFilter !== "All Campaigns" &&
-      ownerNameFilter === "All Owners"
-    ) {
-      leadListForCount = [];
-       campaignIdCount = campgainData.filter(
-        (ele) => ele.name === campaignNameFilter
-      );
-      for (let i = 0; i < campaignIdCount.length; i++) {
-        for (let j = 0; j < genratedLeadData.length; j++) {
-          if (genratedLeadData[j].campaignId === campaignIdCount[i].id) {
-            leadListForCount.push(genratedLeadData[j]);
-          }
-        }
-      }
-    }
-    if (
-      campaignNameFilter === "All Campaigns" &&
-      ownerNameFilter !== "All Owners"
-    ) {
-      leadListForCount = [];
-       campaignIdCount = campgainData.filter(
-        (ele) => ele.owner === ownerNameFilter
-      );
-      for (let i = 0; i < campaignIdCount.length; i++) {
-        for (let j = 0; j < genratedLeadData.length; j++) {
-          if (genratedLeadData[j].campaignId === campaignIdCount[i].id) {
-            leadListForCount.push(genratedLeadData[j]);
-          }
-        }
-      }
-    }
-    if (
-      campaignNameFilter !== "All Campaigns" &&
-      ownerNameFilter !== "All Owners"
-    ) {
-      leadListForCount = [];
-       campaignIdCount = campgainData.filter(
-        (ele) =>
-          ele.name === campaignNameFilter && ele.owner === ownerNameFilter
-      );
-      for (let i = 0; i < campaignIdCount.length; i++) {
-        for (let j = 0; j < genratedLeadData.length; j++) {
-          if (genratedLeadData[j].campaignId === campaignIdCount[i].id) {
-            leadListForCount.push(genratedLeadData[j]);
-          }
-        }
-      }
-    }
-  }
+
 
   if (
     (campaignNameFilter === "" && ownerNameFilter === "") ||
     (campaignNameFilter === "All Campaigns" && ownerNameFilter === "All Owners")
   ) {
-    if (searchDate === "") {
-      const fuse = new Fuse(approveList, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-      filterApprov = searchQuery
-        ? results.map((results) => results.item)
-        : approveList;
-    }
-    if (searchDate !== "") {
-      filterApprovResults = [];
-      for (let i = 0; i < approveList.length; i++) {
-        const start = moment(searchDate.start).format("YYYY-MM-DD");
-        const end = moment(searchDate.end).format("YYYY-MM-DD");
-        const between = moment
-          .unix(approveList[i].leadGeneratedDate.seconds)
-          .format("YYYY-MM-DD");
-        const unixTimestampStart = Math.floor(new Date(start).getTime() / 1000);
-        const unixTimestampBetween = Math.floor(
-          new Date(between).getTime() / 1000
-        );
-        const unixTimestampEnd = Math.floor(new Date(end).getTime() / 1000);
-
-        if (
-          unixTimestampStart < unixTimestampBetween &&
-          unixTimestampBetween < unixTimestampEnd
-        ) {
-          filterApprovResults.push(approveList[i]);
-        }
-      }
-      const fuse = new Fuse(filterApprovResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-      filterApprov = searchQuery
-        ? results.map((results) => results.item)
-        : filterApprovResults;
-    }
+    const campaignIds = campgainData
+    leadListForCount = filterCount(campaignIds,genratedLeadData)
+    filterApprov = filterLeads(campaignIds,approveList,searchDate,searchQuery)
   }
   if (
     (campaignNameFilter === "All Campaigns" || campaignNameFilter === "") &&
     ownerNameFilter !== "All Owners"
   ) {
-    campaignID = campgainData.filter((ele) => ele.owner === ownerNameFilter);
-    if (searchDate === "") {
-      filterApprovResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < approveList.length; j++) {
-          if (approveList[j].campaignId === campaignID[i].id) {
-            filterApprovResults.push(approveList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterApprovResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-      filterApprov = searchQuery
-        ? results.map((results) => results.item)
-        : filterApprovResults;
-    }
-    if (searchDate !== "") {
-      filterApprovResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < approveList.length; j++) {
-          const start = moment(searchDate.start).format("YYYY-MM-DD");
-          const end = moment(searchDate.end).format("YYYY-MM-DD");
-          const between = moment
-            .unix(approveList[i].leadGeneratedDate.seconds)
-            .format("YYYY-MM-DD");
-          const unixTimestampStart = Math.floor(
-            new Date(start).getTime() / 1000
-          );
-          const unixTimestampBetween = Math.floor(
-            new Date(between).getTime() / 1000
-          );
-          const unixTimestampEnd = Math.floor(new Date(end).getTime() / 1000);
-          if (
-            approveList[j].campaignId === campaignID[i].id &&
-            unixTimestampStart < unixTimestampBetween &&
-            unixTimestampBetween < unixTimestampEnd
-          ) {
-            filterApprovResults.push(approveList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterApprovResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-      filterApprov = searchQuery
-        ? results.map((results) => results.item)
-        : filterApprovResults;
-    }
+    const campaignIds = campgainData.filter((ele) => ele.owner === ownerNameFilter);
+    leadListForCount = filterCount(campaignIds,genratedLeadData)
+    filterApprov = filterLeads(campaignIds,approveList,searchDate,searchQuery)
   }
 
   if (
     campaignNameFilter !== "All Campaigns" &&
     ownerNameFilter === "All Owners"
   ) {
-    campaignID = campgainData.filter((ele) => ele.name === campaignNameFilter);
-    if (searchDate === "") {
-      filterApprovResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < approveList.length; j++) {
-          if (approveList[j].campaignId === campaignID[i].id) {
-            filterApprovResults.push(approveList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterApprovResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-      filterApprov = searchQuery
-        ? results.map((results) => results.item)
-        : filterApprovResults;
-    }
-    if (searchDate !== "") {
-      filterApprovResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < approveList.length; j++) {
-          const start = moment(searchDate.start).format("YYYY-MM-DD");
-          const end = moment(searchDate.end).format("YYYY-MM-DD");
-          const between = moment
-            .unix(approveList[i].leadGeneratedDate.seconds)
-            .format("YYYY-MM-DD");
-          const unixTimestampStart = Math.floor(
-            new Date(start).getTime() / 1000
-          );
-          const unixTimestampBetween = Math.floor(
-            new Date(between).getTime() / 1000
-          );
-          const unixTimestampEnd = Math.floor(new Date(end).getTime() / 1000);
-          if (
-            approveList[j].campaignId === campaignID[i].id &&
-            unixTimestampStart < unixTimestampBetween &&
-            unixTimestampBetween < unixTimestampEnd
-          ) {
-            filterApprovResults.push(approveList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterApprovResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-      filterApprov = searchQuery
-        ? results.map((results) => results.item)
-        : filterApprovResults;
-    }
+    const campaignIds = campgainData.filter((ele) => ele.name === campaignNameFilter);
+    leadListForCount = filterCount(campaignIds,genratedLeadData)
+    filterApprov = filterLeads(campaignIds,approveList,searchDate,searchQuery)
   }
 
   if (
     campaignNameFilter !== "All Campaigns" &&
     ownerNameFilter !== "All Owners"
   ) {
-    campaignID = campgainData.filter(
+    const campaignIds = campgainData.filter(
       (ele) => ele.name === campaignNameFilter && ele.owner === ownerNameFilter
     );
-    if (searchDate === "") {
-      filterApprovResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < approveList.length; j++) {
-          if (approveList[j].campaignId === campaignID[i].id) {
-            filterApprovResults.push(approveList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterApprovResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-      filterApprov = searchQuery
-        ? results.map((results) => results.item)
-        : filterApprovResults;
-    }
-    if (searchDate !== "") {
-      filterApprovResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < approveList.length; j++) {
-          const start = moment(searchDate.start).format("YYYY-MM-DD");
-          const end = moment(searchDate.end).format("YYYY-MM-DD");
-          const between = moment
-            .unix(approveList[i].leadGeneratedDate.seconds)
-            .format("YYYY-MM-DD");
-          const unixTimestampStart = Math.floor(
-            new Date(start).getTime() / 1000
-          );
-          const unixTimestampBetween = Math.floor(
-            new Date(between).getTime() / 1000
-          );
-          const unixTimestampEnd = Math.floor(new Date(end).getTime() / 1000);
-          if (
-            approveList[j].campaignId === campaignID[i].id &&
-            unixTimestampStart < unixTimestampBetween &&
-            unixTimestampBetween < unixTimestampEnd
-          ) {
-            filterApprovResults.push(approveList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterApprovResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-      filterApprov = searchQuery
-        ? results.map((results) => results.item)
-        : filterApprovResults;
-    }
+    leadListForCount = filterCount(campaignIds,genratedLeadData)
+    filterApprov = filterLeads(campaignIds,approveList,searchDate,searchQuery)
   }
 
   const rejectList = leadListForCount.filter((ele) => ele.status === -1);
--- src/components/commonComponents/lead/Reject.jsx
+++ src/components/commonComponents/lead/Reject.jsx
@@ -1,16 +1,16 @@
 
-import moment from "moment";
+
 import React from "react";
 import Cards from "./Cards";
-import Fuse from "fuse.js";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-
+import {filterLeads} from "../lead/filterLeads"
 import { getRejectCount } from "../../../redux/actions/approveRejectcount";
 import { getUnderreviewCount } from "../../../redux/actions/approveRejectcount";
 import { getApproveCount } from "../../../redux/actions/approveRejectcount";
 import { getArchieveCount } from "../../../redux/actions/approveRejectcount";
 import { getAllCount } from "../../../redux/actions/approveRejectcount";
+import {filterCount} from "../lead/filterCount"
 import PopupBox from "./PopupBox";
 import "./lead.scss";
 const Reject = () => {
@@ -25,78 +25,10 @@ const Reject = () => {
   const ownerNameFilter = useSelector((state) => state.leadsFilter.ownerName);
   const rejectList = genratedLeadData.filter((ele) => ele.status === -1);
 
-  var filterRejectResults;
+
   var filterReject;
-  var campaignID;
   var leadListForCount;
-  var campaignIdCount;
 
-
-    
-   //For Counting Leads
-
-   if (
-    campaignNameFilter === "All Campaigns" &&
-    ownerNameFilter === "All Owners"
-  ) {
-    leadListForCount = [];
-     leadListForCount = genratedLeadData;
-  }
-  if (
-    campaignNameFilter !== "All Campaigns" ||
-    ownerNameFilter !== "All Owners"
-  ) {
-     
-    if (
-      campaignNameFilter !== "All Campaigns" &&
-      ownerNameFilter === "All Owners"
-    ) {
-      leadListForCount = [];
-       campaignIdCount = campgainData.filter(
-        (ele) => ele.name === campaignNameFilter
-      );
-      for (let i = 0; i < campaignIdCount.length; i++) {
-        for (let j = 0; j < genratedLeadData.length; j++) {
-          if (genratedLeadData[j].campaignId === campaignIdCount[i].id) {
-            leadListForCount.push(genratedLeadData[j]);
-          }
-        }
-      }
-    }
-    if (
-      campaignNameFilter === "All Campaigns" &&
-      ownerNameFilter !== "All Owners"
-    ) {
-      leadListForCount = [];
-       campaignIdCount = campgainData.filter(
-        (ele) => ele.owner === ownerNameFilter
-      );
-      for (let i = 0; i < campaignIdCount.length; i++) {
-        for (let j = 0; j < genratedLeadData.length; j++) {
-          if (genratedLeadData[j].campaignId === campaignIdCount[i].id) {
-            leadListForCount.push(genratedLeadData[j]);
-          }
-        }
-      }
-    }
-    if (
-      campaignNameFilter !== "All Campaigns" &&
-      ownerNameFilter !== "All Owners"
-    ) {
-      leadListForCount = [];
-       campaignIdCount = campgainData.filter(
-        (ele) =>
-          ele.name === campaignNameFilter && ele.owner === ownerNameFilter
-      );
-      for (let i = 0; i < campaignIdCount.length; i++) {
-        for (let j = 0; j < genratedLeadData.length; j++) {
-          if (genratedLeadData[j].campaignId === campaignIdCount[i].id) {
-            leadListForCount.push(genratedLeadData[j]);
-          }
-        }
-      }
-    }
-  }
 
 
 
@@ -104,194 +36,38 @@ const Reject = () => {
     (campaignNameFilter === "" && ownerNameFilter === "") ||
     (campaignNameFilter === "All Campaigns" && ownerNameFilter === "All Owners")
   ) {
-    if(searchDate === ""){
-      const fuse = new Fuse(rejectList, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-       filterReject = searchQuery
-        ? results.map((results) => results.item)
-        : rejectList;
-    }
-    if(searchDate !== ""){
-      
-
-       filterRejectResults = []
-      for (let i = 0; i < rejectList.length; i++){
-        const start = moment(searchDate.start).format("YYYY-MM-DD")
-        const end = moment(searchDate.end).format("YYYY-MM-DD")
-        const between =  moment.unix(rejectList[i].leadGeneratedDate.seconds).format("YYYY-MM-DD")
-        const unixTimestampStart = Math.floor((new Date(start)).getTime() / 1000);
-        const unixTimestampBetween = Math.floor((new Date(between)).getTime() / 1000);
-        const unixTimestampEnd = Math.floor((new Date(end)).getTime() / 1000);
-        
-
-        if(unixTimestampStart < unixTimestampBetween && unixTimestampBetween < unixTimestampEnd){
-          filterRejectResults.push(rejectList[i])
-        }
-      }
-      const fuse = new Fuse(filterRejectResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-       filterReject = searchQuery
-        ? results.map((results) => results.item)
-        : filterRejectResults;
+    let campaignIds = campgainData
+    leadListForCount = filterCount(campaignIds,genratedLeadData)
+    filterReject = filterLeads(campaignIds,rejectList,searchDate,searchQuery)
     
-      
-     
-    }
-    
-
-
   }
   if (
     (campaignNameFilter === "All Campaigns" || campaignNameFilter === "") &&
     ownerNameFilter !== "All Owners"
   ) {
-     campaignID = campgainData.filter(
-      (ele) => ele.owner === ownerNameFilter
-    );
-    if(searchDate === ""){
-       filterRejectResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < rejectList.length; j++) {
-          if (rejectList[j].campaignId === campaignID[i].id) {
-            filterRejectResults.push(rejectList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterRejectResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-       filterReject = searchQuery
-        ? results.map((results) => results.item)
-        : filterRejectResults;
-    }
-    if(searchDate !== ""){
-       filterRejectResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < rejectList.length; j++) {
-          const start = moment(searchDate.start).format("YYYY-MM-DD")
-          const end = moment(searchDate.end).format("YYYY-MM-DD")
-          const between =  moment.unix(rejectList[i].leadGeneratedDate.seconds).format("YYYY-MM-DD")
-          const unixTimestampStart = Math.floor((new Date(start)).getTime() / 1000);
-          const unixTimestampBetween = Math.floor((new Date(between)).getTime() / 1000);
-          const unixTimestampEnd = Math.floor((new Date(end)).getTime() / 1000);
-          if ((rejectList[j].campaignId === campaignID[i].id) && (unixTimestampStart < unixTimestampBetween && unixTimestampBetween < unixTimestampEnd)) {
-            filterRejectResults.push(rejectList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterRejectResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-       filterReject = searchQuery
-        ? results.map((results) => results.item)
-        : filterRejectResults;
-    }
-
+    let campaignIds = campgainData.filter((ele) => ele.owner === ownerNameFilter);
+    leadListForCount = filterCount(campaignIds,genratedLeadData)
+    filterReject = filterLeads(campaignIds,rejectList,searchDate,searchQuery)
   }
 
   if (
     campaignNameFilter !== "All Campaigns" &&
     ownerNameFilter === "All Owners"
   ) {
-     campaignID = campgainData.filter(
-      (ele) => ele.name === campaignNameFilter
-    );
-    if(searchDate === ""){
-       filterRejectResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < rejectList.length; j++) {
-          if (rejectList[j].campaignId === campaignID[i].id) {
-            filterRejectResults.push(rejectList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterRejectResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-       filterReject = searchQuery
-        ? results.map((results) => results.item)
-        : filterRejectResults;
-    }
-    if(searchDate !== ""){
-       filterRejectResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < rejectList.length; j++) {
-          const start = moment(searchDate.start).format("YYYY-MM-DD")
-          const end = moment(searchDate.end).format("YYYY-MM-DD")
-          const between =  moment.unix(rejectList[i].leadGeneratedDate.seconds).format("YYYY-MM-DD")
-          const unixTimestampStart = Math.floor((new Date(start)).getTime() / 1000);
-          const unixTimestampBetween = Math.floor((new Date(between)).getTime() / 1000);
-          const unixTimestampEnd = Math.floor((new Date(end)).getTime() / 1000);
-          if ((rejectList[j].campaignId === campaignID[i].id) && (unixTimestampStart < unixTimestampBetween && unixTimestampBetween < unixTimestampEnd)) {
-            filterRejectResults.push(rejectList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterRejectResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-       filterReject = searchQuery
-        ? results.map((results) => results.item)
-        : filterRejectResults;
-    }
-
+    let campaignIds = campgainData.filter((ele) => ele.name === campaignNameFilter);
+    leadListForCount = filterCount(campaignIds,genratedLeadData)
+    filterReject = filterLeads(campaignIds,rejectList,searchDate,searchQuery)
   }
 
   if (
     campaignNameFilter !== "All Campaigns" &&
     ownerNameFilter !== "All Owners"
   ) {
-     campaignID = campgainData.filter(
+    let campaignIds = campgainData.filter(
       (ele) => ele.name === campaignNameFilter && ele.owner === ownerNameFilter
     );
-    if(searchDate === ""){
-       filterRejectResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < rejectList.length; j++) {
-          if (rejectList[j].campaignId === campaignID[i].id) {
-            filterRejectResults.push(rejectList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterRejectResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-       filterReject = searchQuery
-        ? results.map((results) => results.item)
-        : filterRejectResults;
-    }
-    if(searchDate !== ""){
-       filterRejectResults = [];
-      for (let i = 0; i < campaignID.length; i++) {
-        for (let j = 0; j < rejectList.length; j++) {
-          const start = moment(searchDate.start).format("YYYY-MM-DD")
-          const end = moment(searchDate.end).format("YYYY-MM-DD")
-          const between =  moment.unix(rejectList[i].leadGeneratedDate.seconds).format("YYYY-MM-DD")
-          const unixTimestampStart = Math.floor((new Date(start)).getTime() / 1000);
-          const unixTimestampBetween = Math.floor((new Date(between)).getTime() / 1000);
-          const unixTimestampEnd = Math.floor((new Date(end)).getTime() / 1000);
-          if ((rejectList[j].campaignId === campaignID[i].id) && (unixTimestampStart < unixTimestampBetween && unixTimestampBetween < unixTimestampEnd)) {
-            filterRejectResults.push(rejectList[j]);
-          }
-        }
-      }
-      const fuse = new Fuse(filterRejectResults, {
-        keys: ["title", "summary", "companyName"],
-      });
-      const results = fuse.search(searchQuery);
-       filterReject = searchQuery
-        ? results.map((results) => results.item)
-        : filterRejectResults;
-    }
+    leadListForCount = filterCount(campaignIds,genratedLeadData)
+    filterReject = filterLeads(campaignIds,rejectList,searchDate,searchQuery)
   }
 
   const rejectListCount = leadListForCount.filter((ele) => ele.status === -1);
