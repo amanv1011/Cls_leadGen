a45feb0da775df3060256713842d3c6fa5efc9c6
Onkar Singh Rao <onkar.rao@classicinformatics.com>
2022-06-01 12:24:21 +0530

    createed layout for new lead page

--- src/components/commonComponents/leadDescription/index.jsx
+++ src/components/commonComponents/leadDescription/index.jsx
@@ -1,10 +1,22 @@
-import { Divider } from "@mui/material";
+import { Avatar, Divider, IconButton } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import "./leadDescription.scss";
 
 const desc =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+const obj = {
+  campaignId: "PmrXt2VjGEPeU28xDWcm",
+  companyName: "Barclays",
+  name: "Onkar",
+  id: "fnho3mrM1a41ndJhJYms",
+  location: "Glasgow G2",
+  readMore:
+    "https://uk.indeed.com/rc/clk?jk=f5278a4f52cfc421&fccid=057abf3fd357e717&vjs=3",
+  status: 0,
+  summary: "Develop the skills to code, whatever your background.",
+  title: "Aspiring Developer Analyst - Expert Graduate Programme - 202...",
+};
 const LeadDescription = () => {
   return (
     <Box component={"div"} className="leads-description-container">
@@ -19,19 +31,55 @@ const LeadDescription = () => {
         </Box>
         <Box component={"div"} className="right-section">
           <Box component={"div"} className="links">
-            Links
+            <Box component={"div"} className="link">
+              <IconButton>
+                {/* <a href={ele.readMore} target="_blank"> */}
+                <Avatar
+                  sx={{ width: "24px", height: "24px" }}
+                  alt="indeedLogo"
+                  // src={indeedLogo3}
+                  className="indeed-logo"
+                />
+                {/* </a> */}
+              </IconButton>
+              Company
+            </Box>
+            <Box component={"div"} className="link">
+              <IconButton>
+                {/* <a href={ele.readMore} target="_blank"> */}
+                <Avatar
+                  sx={{ width: "24px", height: "24px" }}
+                  alt="indeedLogo"
+                  // src={indeedLogo3}
+                  className="indeed-logo"
+                />
+                {/* </a> */}
+              </IconButton>
+              People
+            </Box>
+            <Box component={"div"} className="link">
+              <IconButton>
+                {/* <a href={ele.readMore} target="_blank"> */}
+                <Avatar
+                  sx={{ width: "24px", height: "24px" }}
+                  alt="indeedLogo"
+                  // src={indeedLogo3}
+                  className="indeed-logo"
+                />
+                {/* </a> */}
+              </IconButton>
+              Details
+            </Box>
           </Box>
         </Box>
       </Box>
-      <Divider />
+      <Divider
+        variant="middle"
+        light="true"
+        sx={{ height: "1px", background: "#1F4173", opacity: "0.15" }}
+      />
       <Box component={"div"} className="leads-description-body">
         <Box component={"div"} className="leads-description-heading">
-          <Box component={"div"} className="heading-element">
-            Name : "Onkar Singh"
-          </Box>
-          <Box component={"div"} className="heading-element">
-            Name : "Onkar Singh"
-          </Box>
           <Box component={"div"} className="heading-element">
             Name : "Onkar Singh"
           </Box>
--- src/components/pageComponents/leads2/index.jsx
+++ src/components/pageComponents/leads2/index.jsx
@@ -1,3 +1,4 @@
+import { Divider } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import LeadDescription from "../../commonComponents/leadDescription";
@@ -6,11 +7,37 @@ import LeadsHeader from "../../themeComponents/header/leadsHeader/leadsHeader";
 import "./leads.scss";
 
 const Leads2 = () => {
+  //Onkar's Workspace
+
+  /*getting description of sigle lead onclick
+  const openPopup = (e) => {
+    e.preventDefault();
+    let leadsId = e.currentTarget.id;
+    let leadsIdData = leadsData.filter((ele) => ele.id === leadsId);
+    dispatch(getPopupEnable(leadsIdData));
+  };*/
+
+  // let approveButton = (event) => {
+  //   dispatch(updateLeadStatus(event.target.value, 1));
+  // };
+  // let rejectButton = (event) => {
+  //   dispatch(updateLeadStatus(event.target.value, -1));
+  // };
+  // let archieveButton = (event) => {
+  //   dispatch(updateLeadStatus(event.target.value, 2));
+  // };
+
+  //Onkar's Workspace
+
   return (
     <Box component="div" className="leads-container">
       <Box component={"div"} className="leads-header">
         <LeadsHeader />
       </Box>
+      <Divider
+        light="true"
+        sx={{ height: "1px", background: "#1F4173", opacity: "0.15" }}
+      />
       <Box component={"div"} className="leads-body">
         <Box component={"div"} className="section leads-list">
           List Here
@@ -19,31 +46,36 @@ const Leads2 = () => {
           <LeadDescription />
         </Box>
         <Box component={"div"} className="section leads-actions">
-          Leads Action Here
-          <IButton
-            type={"green"}
-            name={"Approve"}
-            children="Approve"
-            onclick={console.log("e")}
-          />
-          <IButton
-            type={"yellow"}
-            name={"Archive"}
-            children="Archive"
-            onclick={console.log("e")}
-          />
-          <IButton
-            type={"grey"}
-            name={"Under Review"}
-            children="Under Review"
-            onclick={console.log("e")}
-          />
-          <IButton
-            type={"pink"}
-            name={"Reject"}
-            children="Reject"
-            onclick={console.log("e")}
-          />
+          <Box component={"div"} className="action-title">
+            Move To:
+          </Box>
+          <Box component={"div"} className="action-buttons">
+            <IButton
+              type={"green"}
+              name={"Approve"}
+              children="Approve"
+              onclick={() => console.log("e")}
+            />
+            <IButton
+              type={"yellow"}
+              name={"Archive"}
+              children="Archive"
+              onclick={() => console.log("e")}
+            />
+            <IButton
+              type={"grey"}
+              name={"Under Review"}
+              children="Under Review"
+              onclick={() => console.log("e")}
+            />
+            <IButton
+              type={"pink"}
+              name={"Reject"}
+              children="Reject"
+              customClass={"disabled"}
+              onclick={() => console.log("e")}
+            />
+          </Box>
         </Box>
       </Box>
     </Box>
