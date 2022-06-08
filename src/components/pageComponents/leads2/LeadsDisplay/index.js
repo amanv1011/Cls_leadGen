4936767e387bc60c17ee8caa9f695269026efd7d
Mithun Dominic <mithun.dominic@gmail.com>
2022-06-08 09:47:53 +0530

    Added bulk select for leads

--- src/components/pageComponents/leads2/LeadsDisplay/index.js
+++ src/components/pageComponents/leads2/LeadsDisplay/index.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LeadsCheckbox from "../LeadsCheckbox";
 import "./leadsDisplay.scss";
 import { useDispatch } from "react-redux";
 import { getPopupEnable } from "../../../../redux/actions/PopupAction";
+import LeadsMenu from "../LeadsMenu";
 
 const LeadsDisplay = ({ leadsList, selectedLeadIdFun }) => {
   const dispatch = useDispatch();
+
+  const [leadsListData, setLeadsListData] = useState([]);
+
+  useEffect(() => {
+    setLeadsListData(leadsList);
+  }, [leadsList]);
 
   const handleClick = (leadId) => {
     let leadsIdData = leadsList.filter((ele) => ele.id === leadId);
@@ -13,20 +20,64 @@ const LeadsDisplay = ({ leadsList, selectedLeadIdFun }) => {
     dispatch(getPopupEnable(leadsIdData));
   };
 
+  const handleOnCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    if (name === "allCheck") {
+      let tempLeadsArray = leadsListData.map((lead) => ({
+        ...lead,
+        isChecked: checked,
+      }));
+      setLeadsListData(tempLeadsArray);
+    } else {
+      let tempLeadsArray = leadsListData.map((lead) =>
+        lead.id === name ? { ...lead, isChecked: checked } : lead
+      );
+      setLeadsListData(tempLeadsArray);
+    }
+  };
+
+  console.log("leadsListData", leadsListData);
+
   return (
     <React.Fragment>
+      <div
+        style={{
+          display: "flex",
+          alignContent: "center",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <input
+            type="checkbox"
+            name="allCheck"
+            checked={
+              leadsListData.filter((lead) => lead?.isChecked !== true).length <
+              1
+            }
+            onChange={handleOnCheckboxChange}
+          />
+          <label>All</label>
+        </div>
+        <LeadsMenu />
+      </div>
       <div className="lead-display-container">
-        {leadsList &&
-          leadsList.map((lead) => (
-            <div
-              className="lead-display-subcontainers"
-              onClick={() => handleClick(lead.id)}
-              key={lead.id}
-            >
+        {leadsListData &&
+          leadsListData.map((lead) => (
+            <div className="lead-display-subcontainers" key={lead.id}>
               <div className="lead-display-subcontainer1">
-                <LeadsCheckbox />
-                <p className="lead-display-btn-text">
-                  {lead.title.slice(0, 15)}
+                <input
+                  type="checkbox"
+                  name={lead.id}
+                  checked={lead?.isChecked || false}
+                  onChange={handleOnCheckboxChange}
+                />
+                <p
+                  className="lead-display-btn-text"
+                  onClick={() => handleClick(lead.id)}
+                >
+                  {lead.title && lead.title.slice(0, 15)}
                 </p>
                 <p>Gene</p>
               </div>
--- src/redux/actions/approveRejectcount.js
+++ src/redux/actions/approveRejectcount.js
@@ -1,36 +1,37 @@
-import {GET_APPROVE_COUNT,
-    GET_REJECT_COUNT,
-    GET_UNDERREVIEW_COUNT,
-    GET_ARCHIEVE_COUNT,
-    GET_ALL_COUNT
-} from "../type"
+import {
+  GET_APPROVE_COUNT,
+  GET_REJECT_COUNT,
+  GET_UNDERREVIEW_COUNT,
+  GET_ARCHIEVE_COUNT,
+  GET_ALL_COUNT,
+} from "../type";
 
-export const getApproveCount = (count) =>{
-    return async(dispatch) => {
-        dispatch({type:GET_APPROVE_COUNT, payload:count})
-    }
-}
+export const getApproveCount = (count) => {
+  return async (dispatch) => {
+    dispatch({ type: GET_APPROVE_COUNT, payload: count });
+  };
+};
 
-export const getRejectCount = (count) =>{
-    return async(dispatch) => {
-        dispatch({type:GET_REJECT_COUNT, payload:count})
-    }
-}
+export const getRejectCount = (count) => {
+  return async (dispatch) => {
+    dispatch({ type: GET_REJECT_COUNT, payload: count });
+  };
+};
 
-export const getUnderreviewCount = (count) =>{
-    return async(dispatch) => {
-        dispatch({type:GET_UNDERREVIEW_COUNT, payload:count})
-    }
-}
+export const getUnderreviewCount = (count) => {
+  return async (dispatch) => {
+    dispatch({ type: GET_UNDERREVIEW_COUNT, payload: count });
+  };
+};
 
-export const getArchieveCount = (count) =>{
-    return async(dispatch) => {
-        dispatch({type:GET_ARCHIEVE_COUNT, payload:count})
-    }
-}
+export const getArchieveCount = (count) => {
+  return async (dispatch) => {
+    dispatch({ type: GET_ARCHIEVE_COUNT, payload: count });
+  };
+};
 
 export const getAllCount = (count) => {
-    return async(dispatch) => {
-        dispatch({type:GET_ALL_COUNT, payload:count})
-    }
-}
+  return async (dispatch) => {
+    dispatch({ type: GET_ALL_COUNT, payload: count });
+  };
+};
