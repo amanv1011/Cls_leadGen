47f30a10e30c3e8ae897317ff286b84dc828a98e
ambar_gupta <ambar.gupta@classicinformatics.com>
2022-06-24 18:06:16 +0530

    added token verification on app.js

--- src/components/pageComponents/unAuthorized/index.js
+++ src/components/pageComponents/unAuthorized/index.js
@@ -8,7 +8,7 @@ const UnAuthorizedComponent = () => {
     return (
         <div className="redirect-box">
             <h4>Please Login on Portal's Dashboard, And try again</h4>
-            <a href={loginUrl.local} className="login-link">Click here to Login</a>
+            <a href={loginUrl.staging} className="login-link">Click here to Login</a>
         </div>
     )
 }
