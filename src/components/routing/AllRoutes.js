13d68511fd50db939d964a0e6e6484b9ac2a4f9a
Monu Kumar <monu.kumar@classicinformatics.com>
2022-07-07 09:37:15 +0530

    fixed sorting issues, clear date range while click on clear filter

--- src/components/routing/AllRoutes.js
+++ src/components/routing/AllRoutes.js
@@ -82,8 +82,8 @@ const AllRoutes = () => {
       element: <Navigate to="/" />,
     },
     {
-      path: "unAuthorized",
-      element: <UnAuthorizedComponent></UnAuthorizedComponent>,
+      // path: "unAuthorized",
+      // element: <UnAuthorizedComponent></UnAuthorizedComponent>,
     },
   ]);
   return allRoutes;
