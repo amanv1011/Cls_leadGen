d578f404e899a348bbefd9f489b5d6a10151f38d
Mithun Dominic <mithun.dominic@gmail.com>
2022-07-26 18:05:08 +0530

    Removed the logged in user from assign list

--- src/components/pageComponents/campaign/CampaignDescription/index.js
+++ src/components/pageComponents/campaign/CampaignDescription/index.js
@@ -46,14 +46,7 @@ const CampaignDescription = ({
   lastCrawledDateList,
   selectedArray,
   campaignsListData,
-  setcampaignsListData,
-  countryFilterValue,
-  ownerFilterValue,
-  campaignStateFilterValue,
-  assignedCampaigns,
-  campaignsList,
-  options,
-  campaignSateStatus,
+  loggedInUser,
 }) => {
   const dispatch = useDispatch();
 
@@ -787,8 +780,11 @@ const CampaignDescription = ({
                   >
                     Assign To
                   </Box>
+
                   <AssignCampaign
-                    options={allUsers}
+                    options={allUsers.filter(
+                      (user) => user.userId !== loggedInUser.id
+                    )}
                     onChangeOption={onChangeOption}
                     selectedUsers={selectedUsers}
                     selectedArray={selectedArray}
