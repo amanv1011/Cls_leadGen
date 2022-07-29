ba8bee8b29dc285821aa74a34956a2732339579b
ambar_gupta <ambar.gupta@classicinformatics.com>
2022-07-29 10:57:24 +0530

    updating code for production

--- src/components/pageComponents/unAuthorized/index.js
+++ src/components/pageComponents/unAuthorized/index.js
@@ -1,13 +1,13 @@
 import './unAuthorized.css'
 const UnAuthorizedComponent = () => {
     const {
-        REACT_APP_APPLICATION_LINK
+        REACT_APP_REDIRECT_LINK
     } = process.env
 
     return (
         <div className="redirect-box">
             <h4>Please Login on Portal's Dashboard, And try again</h4>
-            <a href={REACT_APP_APPLICATION_LINK} className="login-link">Click here to Login</a>
+            <a href={REACT_APP_REDIRECT_LINK} className="login-link">Click here to Login</a>
         </div>
     )
 }
