cdde25e07c48bbf02816440d3ec1a97b05a1f90c
Mithun <mithun.dominic@classicinformatics.com>
2022-10-10 10:28:46 +0530

    Modified ternary operator for return ,UI - blocked companies LAN-147

--- src/components/commonComponents/BlockedLeadsTable/index.js
+++ src/components/commonComponents/BlockedLeadsTable/index.js
@@ -40,7 +40,7 @@ export const BlockedLeadsTable = () => {
     dispatch(getBlockedCompaniesListAction());
   }, []);
 
-  return blockedCompaniesList?.length === 0 ? (
+  return (
     <Box component="div" className="campaign-container">
       <Box component={"div"} className="campaign-body">
         <Box
@@ -51,75 +51,64 @@ export const BlockedLeadsTable = () => {
           }}
           className="section campaign-details"
         >
-          <div
-            style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              fontSize: "20px",
-            }}
-          >
-            No Blocked Companies
-          </div>
-        </Box>
-      </Box>
-    </Box>
-  ) : (
-    <Box component="div" className="campaign-container">
-      <Box component={"div"} className="campaign-body">
-        <Box
-          component={"div"}
-          style={{
-            height: " 87vh",
-            width: "87vw",
-          }}
-          className="section campaign-details"
-        >
-          <Paper sx={{ width: "100%", height: "100%" }}>
-            <TableContainer sx={{ maxHeight: 555 }}>
-              <Table stickyHeader aria-label="sticky table">
-                <TableHead>
-                  <TableRow>
-                    {columns.map((column) => (
-                      <TableCell
-                        key={column.id}
-                        align={column.align}
-                        style={{ minWidth: column.minWidth }}
-                      >
-                        {column.label}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {blockedCompaniesList.map((blockedCompany) => {
-                    return (
-                      <TableRow
-                        hover
-                        role="checkbox"
-                        tabIndex={-1}
-                        key={blockedCompany.id}
-                      >
-                        {columns.map((column) => {
-                          const value =
-                            column.id === "leadId"
-                              ? blockedCompany[column.id].length
-                              : blockedCompany[column.id];
-                          return (
-                            <TableCell key={column.id} align={column.align}>
-                              {column.format && typeof value === "number"
-                                ? column.format(value)
-                                : value}
-                            </TableCell>
-                          );
-                        })}
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Paper>
+          {blockedCompaniesList?.length === 0 ? (
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                fontSize: "20px",
+              }}
+            >
+              No Blocked Companies
+            </div>
+          ) : (
+            <Paper sx={{ width: "100%", height: "100%" }}>
+              <TableContainer sx={{ maxHeight: 555 }}>
+                <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                    <TableRow>
+                      {columns.map((column) => (
+                        <TableCell
+                          key={column.id}
+                          align={column.align}
+                          style={{ minWidth: column.minWidth }}
+                        >
+                          {column.label}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {blockedCompaniesList.map((blockedCompany) => {
+                      return (
+                        <TableRow
+                          hover
+                          role="checkbox"
+                          tabIndex={-1}
+                          key={blockedCompany.id}
+                        >
+                          {columns.map((column) => {
+                            const value =
+                              column.id === "leadId"
+                                ? blockedCompany[column.id].length
+                                : blockedCompany[column.id];
+                            return (
+                              <TableCell key={column.id} align={column.align}>
+                                {column.format && typeof value === "number"
+                                  ? column.format(value)
+                                  : value}
+                              </TableCell>
+                            );
+                          })}
+                        </TableRow>
+                      );
+                    })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Paper>
+          )}
         </Box>
       </Box>
     </Box>
